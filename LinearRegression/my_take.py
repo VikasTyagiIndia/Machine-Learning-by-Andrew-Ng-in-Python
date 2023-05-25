61f1efd7d76a542e6a44c8b20b1623de3caea905
VikasTyagiIndia <100580991+VikasTyagiIndia@users.noreply.github.com>
2023-05-25 11:31:13 +0530

    Update my_take.py

--- LinearRegression/my_take.py
+++ LinearRegression/my_take.py
@@ -1,1 +1,8 @@
+# Importing basic modules
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# Importing data
+data = pd.read_csv("Uni_linear.txt", Header= None)
+print(data)
